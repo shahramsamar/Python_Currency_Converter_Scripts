89ad0ddd56ab8fecf6552badcdc445afc557ad58
shahram <shahramsamar2010@gmail.com>
2024-05-18 10:44:32 +0330

    Update main.py

---
+++ core/main.py
@@ -0,0 +1,24 @@
+import requests
+
+
+
+input_amount = float(input("Enter  the amount of your currency :"))
+base_currency = input("Enter  the amount of your currency :")
+target_currency = input("Enter  the amount of your currency :")
+
+url ="https://api.coingecko.com/api/v3/simple/price"
+
+params = {
+    "ids":base_currency,
+    "vs_currencies":target_currency
+}
+
+response = requests.get(url,params=params)
+
+data = response.json()
+
+base_target_amount = data[base_currency][target_currency]
+
+result = input_amount * base_target_amount
+
+print("result:",result)
